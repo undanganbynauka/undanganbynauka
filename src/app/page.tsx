2ce835d39b2669f05e9b5832e8387b0356b6bb2a
undanganbynauka <undanganbynauka@gmail.com>
2026-06-20 10:44:29 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,1 @@
-import Link from "next/link";
 
-export default function Home() {
-  return (
-    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh", gap: "20px" }}>
-      <h1>Nauka</h1>
-
-      <Link href="/sacred">Sacred</Link>
-      <Link href="/celestial">Celestial</Link>
-    </main>
-  );
-}

ab495aac3f74ea707518abe5fa534a68aeb27ab2
undanganbynauka <undanganbynauka@gmail.com>
2026-06-29 10:45:05 +0700

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -4,13 +4,7 @@ export const runtime = "edge";
 export const dynamic = "force-static";
 
 export async function GET() {
-  // Nanti data ini diambil dari database
   const groomName = "Angga";
-  const brideName = "Rini";
-  const weddingDate = "12 Desember 2026";
-
-  const groomInitial = groomName.charAt(0).toUpperCase();
-  const brideInitial = brideName.charAt(0).toUpperCase();
 
   return new ImageResponse(
     (
@@ -29,9 +23,9 @@ export async function GET() {
         <div
           style={{
             color: "#D4AF37",
-            fontSize: 24,
-            letterSpacing: 8,
-            marginBottom: 28,
+            fontSize: 26,
+            letterSpacing: 6,
+            marginBottom: 24,
           }}
         >
           THE WEDDING OF
@@ -40,32 +34,31 @@ export async function GET() {
         <div
           style={{
             color: "#F8E7A1",
-            fontSize: 140,
+            fontSize: 110,
             fontWeight: 700,
-            lineHeight: 1,
           }}
         >
-          {groomInitial} &amp; {brideInitial}
+          A &amp; R
         </div>
 
         <div
           style={{
-            color: "#FFFFFF",
-            fontSize: 42,
-            marginTop: 24,
+            color: "#F8F8F8",
+            fontSize: 38,
+            marginTop: 22,
           }}
         >
-          {groomName} &amp; {brideName}
+          Angga &amp; Rini
         </div>
 
         <div
           style={{
-            color: "#D4AF37",
+            color: "#C9A227",
             fontSize: 24,
             marginTop: 18,
           }}
         >
-          {weddingDate}
+          12 Desember 2026
         </div>
       </div>
     ),

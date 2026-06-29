d22ae0cc03b9660efffc8bd3c1724e56231ffe17
undanganbynauka <undanganbynauka@gmail.com>
2026-06-29 10:51:24 +0700

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -4,7 +4,7 @@ export const runtime = "edge";
 export const dynamic = "force-static";
 
 export async function GET() {
-  const groomName = "Angga";
+  const text = "Angga";
 
   return new ImageResponse(
     (
@@ -13,53 +13,14 @@ export async function GET() {
           width: "100%",
           height: "100%",
           display: "flex",
-          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           backgroundColor: "#0F172A",
           color: "#FFFFFF",
+          fontSize: 60,
         }}
       >
-        <div
-          style={{
-            color: "#D4AF37",
-            fontSize: 26,
-            letterSpacing: 6,
-            marginBottom: 24,
-          }}
-        >
-          THE WEDDING OF
-        </div>
-
-        <div
-          style={{
-            color: "#F8E7A1",
-            fontSize: 110,
-            fontWeight: 700,
-          }}
-        >
-          A &amp; R
-        </div>
-
-        <div
-          style={{
-            color: "#F8F8F8",
-            fontSize: 38,
-            marginTop: 22,
-          }}
-        >
-          {groomName} &amp; Rini
-        </div>
-
-        <div
-          style={{
-            color: "#C9A227",
-            fontSize: 24,
-            marginTop: 18,
-          }}
-        >
-          12 Desember 2026
-        </div>
+        {text}
       </div>
     ),
     {

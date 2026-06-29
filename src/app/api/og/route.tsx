b7330d81bda5cff91d5f7678705cfa5656fb846e
undanganbynauka <undanganbynauka@gmail.com>
2026-06-29 10:24:22 +0700

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -5,30 +5,6 @@ export const dynamic = "force-static";
 
 export async function GET(req: Request) {
   const url = new URL(req.url);
-  const params = url.searchParams;
 
-  const groom = params.get("groom") || "A";
-
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#1b2333",
-          color: "#e7c97f",
-          fontSize: 52,
-        }}
-      >
-        {groom}
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-    }
-  );
+  return new Response(url.searchParams.get("groom") ?? "NULL");
 }

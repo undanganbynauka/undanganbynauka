cba236ad6e84c96e26e6066bde987ea744bfe0b5
undanganbynauka <undanganbynauka@gmail.com>
2026-06-29 09:38:40 +0700

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,22 +1,29 @@
-return new ImageResponse(
-  (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        backgroundColor: "#1b2333",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "#e7c97f",
-        fontSize: 52,
-      }}
-    >
-      The Wedding Of
-    </div>
-  ),
-  {
-    width: 1200,
-    height: 630,
-  }
-);
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const dynamic = "force-static";
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1b2333",
+          color: "#e7c97f",
+          fontSize: 52,
+        }}
+      >
+        The Wedding Of
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}

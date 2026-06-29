c0b2ffdb03a93c7427c990414b138b795c9db279
undanganbynauka <undanganbynauka@gmail.com>
2026-06-29 10:29:46 +0700

    Update route.tsx

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,10 +1,33 @@
 import { ImageResponse } from "next/og";
+import { NextRequest } from "next/server";
 
 export const runtime = "edge";
 export const dynamic = "force-static";
 
-export async function GET(req: Request) {
-  const url = new URL(req.url);
+export async function GET(request: NextRequest) {
+  const groom = request.nextUrl.searchParams.get("groom") ?? "A";
+  const bride = request.nextUrl.searchParams.get("bride") ?? "L";
 
-  return new Response(url.searchParams.get("groom") ?? "NULL");
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "#1b2333",
+          color: "#e7c97f",
+          fontSize: 52,
+        }}
+      >
+        {groom} &amp; {bride}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
 }
